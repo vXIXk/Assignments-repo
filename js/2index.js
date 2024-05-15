fa5f598f056f5988e926eac4141be5e93371cd5e
vXIXk <vlada.kostenko.19@gmail.com>
2024-05-16 01:00:07 +0300

    Initial assignment commit

---
+++ js/2index.js
@@ -0,0 +1,19 @@
+//* 1‣ Задане значення isDay (true - зараз день, false - зараз ніч).
+
+/* let isDay = false;
+isDay
+      ? document.write(
+              `<p style="margin:2.5rem; color:#070f2b;">Світла тема</p>`
+        )
+      : document.write(
+              `<body style="background-color:#070f2b;"><p style="margin:2.5rem; color:#eeeeee;">Темна тема</p></body>`
+        ); */
+
+//* 2‣ Відомо, чи користувач онлайн (isOnline).
+
+/* let isOnline = true;
+if (!isOnline) {
+      document.write(
+            `<p style="font-size:1.5rem; margin:2.5rem;">Ваше повідомлення буде відправлено при під'єднанні до мережі Інтернет.</p></body>`
+      );
+}; */
