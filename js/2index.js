e4c16b8c3b1a8cfa08ecf184eaeb4e1fa7748c92
vXIXk <vlada.kostenko.19@gmail.com>
2024-05-16 13:01:37 +0300

    Add to conditionals assignment

--- js/2index.js
+++ js/2index.js
@@ -1,7 +1,7 @@
 //* 1‣ Задане значення isDay (true - зараз день, false - зараз ніч).
 
-/* let isDay = false;
-isDay
+/* const isDay = false;
+ isDay
       ? document.write(
               `<p style="margin:2.5rem; color:#070f2b;">Світла тема</p>`
         )
@@ -11,9 +11,22 @@ isDay
 
 //* 2‣ Відомо, чи користувач онлайн (isOnline).
 
-/* let isOnline = true;
-if (!isOnline) {
+/* const isOnline = true;
+ if (!isOnline) {
       document.write(
             `<p style="font-size:1.5rem; margin:2.5rem;">Ваше повідомлення буде відправлено при під'єднанні до мережі Інтернет.</p></body>`
       );
 }; */
+
+//* 3‣ Користувач вводить суму покупки.
+
+/* const userSum = +prompt("Введіть суму покупки:");
+if (userSum >= 800) {
+      let sum = userSum - userSum * 0.05;
+      alert(`Сума вашої покупки — ${sum} грн.`);
+} else if (userSum < 800 && userSum >= 500) {
+      let sum = userSum - userSum * 0.03;
+      alert(`Сума вашої покупки — ${sum} грн.`);
+} else {
+      alert(`Сума вашої покупки — ${userSum} грн.`);
+} */
