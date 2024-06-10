818e69085546cdda161109e475e941da8baa5598
vXIXk <vlada.kostenko.19@gmail.com>
2024-06-10 21:42:31 +0300

    Finish(?) last week's tasks

--- js/8index.js
+++ js/8index.js
@@ -1,34 +1,35 @@
+"use strict";
+
 //* Recursion
 // Task 1
 
 function pow(base, exponent) {
-      if (typeof base !== "number" || typeof exponent !== "number") {
-            throw new TypeError("Input values must be numbers.");
-      }
-      if (exponent < -100 || exponent > 100) {
-            throw new RangeError(
-                  "Exponent value not allowed to be greater than / less than ± 100.",
-            );
-      }
-      switch (true) {
-            case exponent === 1: {
-                  return base;
+      try {
+            if (typeof base !== "number" || typeof exponent !== "number") {
+                  throw new TypeError("Input values must be numbers.");
             }
-            case exponent === 0: {
-                  return 1;
+            if (exponent < -100 || exponent > 100) {
+                  throw new RangeError(
+                        "Exponent value not allowed to be greater than / less than ± 100.",
+                  );
             }
-            case exponent < 0 && exponent >= -100: {
-                  return base ** exponent;
+            switch (true) {
+                  case exponent === 1: {
+                        return base;
+                  }
+                  case exponent === 0: {
+                        return 1;
+                  }
+                  case exponent < 0 && exponent >= -100: {
+                        return base ** exponent;
+                  }
             }
+            return base * pow(base, --exponent);
+      } catch (err) {
+            return err;
       }
-      return base * pow(base, --exponent);
 }
-
-try {
-      console.log(pow(19, 2));
-} catch (err) {
-      console.log(err);
-}
+console.log(pow(19, 2));
 
 // Task 2
 
@@ -56,4 +57,20 @@ function validateEmail(email) {
 
             ${email} is not a valid E-mail address.`;
       }
+}
+
+//* Closure
+
+const number = +prompt("Put your starter number here! ↯");
+const step = +prompt("And the step here ↯");
+
+function count(number, step) {
+      return function () {
+            return (number += step);
+      };
+}
+const counter = count(number, step);
+
+for (let i = 0; i <= 1000; i++) {
+      console.log(counter());
 }
