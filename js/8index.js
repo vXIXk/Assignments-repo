687245ee8333136e26e97711723b5fb685d6ea8a
vXIXk <vlada.kostenko.19@gmail.com>
2024-06-12 12:16:53 +0300

    Move try catch

--- js/8index.js
+++ js/8index.js
@@ -4,59 +4,55 @@
 // Task 1
 
 function pow(base, exponent) {
-      try {
-            if (typeof base !== "number" || typeof exponent !== "number") {
-                  throw new TypeError("Input values must be numbers.");
-            }
-            if (exponent < -100 || exponent > 100) {
-                  throw new RangeError(
-                        "Exponent value not allowed to be greater than / less than ± 100.",
-                  );
-            }
-            switch (true) {
-                  case exponent === 1: {
-                        return base;
-                  }
-                  case exponent === 0: {
-                        return 1;
-                  }
-                  case exponent < 0 && exponent >= -100: {
-                        return base ** exponent;
-                  }
-            }
-            return base * pow(base, --exponent);
-      } catch (err) {
-            return err;
+      if (typeof base !== "number" || typeof exponent !== "number") {
+            throw new TypeError("Input values must be numbers.");
       }
+      if (exponent < -100 || exponent > 100) {
+            throw new RangeError(
+                  "Exponent value not allowed to be greater than / less than ± 100.",
+            );
+      }
+      switch (true) {
+            case exponent === 1: {
+                  return base;
+            }
+            case exponent === 0: {
+                  return 1;
+            }
+            case exponent < 0 && exponent >= -100: {
+                  return base ** exponent;
+            }
+      }
+      return base * pow(base, --exponent);
 }
-console.log(pow(19, 2));
+try {
+      console.log(pow(19, 2));
+} catch (err) {
+      return err;
+}
 
 // Task 2
 
 function validateEmail(email) {
-      try {
-            if (typeof email !== "string") {
-                  throw new TypeError(
-                        "Input value must be of the 'string' type to be valid.",
-                  );
-            }
-            if (!email.includes("@")) {
-                  throw new Error(
-                        "An email address must include the at sign (@).",
-                  );
-            }
-            if (
-                  email.indexOf("@") === 0 ||
-                  email.indexOf("@") === email.length--
-            ) {
-                  throw new Error("Incorrect use of the at sign (@).");
-            }
-            return `${email} is a valid E-mail address`;
-      } catch (err) {
-            return `${err}
-
-            ${email} is not a valid E-mail address.`;
+      if (typeof email !== "string") {
+            throw new TypeError(
+                  "Input value must be of the 'string' type to be valid.",
+            );
       }
+      if (!email.includes("@")) {
+            throw new Error("An email address must include the at sign (@).");
+      }
+      if (email.indexOf("@") === 0 || email.indexOf("@") === email.length--) {
+            throw new Error("Incorrect use of the at sign (@).");
+      }
+      return `${email} is a valid E-mail address`;
+}
+try {
+      console.log(validateEmail(`vlada.kostenko.19@gmail.com`));
+} catch (err) {
+      return `${err}
+
+      ${email} is not a valid E-mail address.`;
 }
 
 //* Closure
