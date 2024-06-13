e09de65f3be993a4c14563dc3a97bb768aa7f548
vXIXk <vlada.kostenko.19@gmail.com>
2024-06-13 13:00:56 +0300

    Remove switch cases

--- js/8index.js
+++ js/8index.js
@@ -12,16 +12,11 @@ function pow(base, exponent) {
                   "Exponent value not allowed to be greater than / less than ± 100.",
             );
       }
-      switch (true) {
-            case exponent === 1: {
-                  return base;
-            }
-            case exponent === 0: {
-                  return 1;
-            }
-            case exponent < 0 && exponent >= -100: {
-                  return base ** exponent;
-            }
+      if (exponent === 0) {
+            return 1;
+      }
+      if (exponent < 0 && exponent >= -100) {
+            return base ** exponent;
       }
       return base * pow(base, --exponent);
 }
