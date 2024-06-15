18330cf39e15055221c84188683a27c2d50a0086
vXIXk <vlada.kostenko.19@gmail.com>
2024-06-15 10:12:35 +0300

    Update file

--- DOM/index.js
+++ DOM/index.js
@@ -12,13 +12,3 @@ image.onmouseenter = e =>
       (image.src =
             "https://i.pinimg.com/564x/69/d5/d7/69d5d7727c1e56acfd136f7e0d76a2ac.jpg");
 image.onmouseleave = e => (image.src = ogImgSrc);
-
-const user = {
-      firstName: "Test",
-      lastName: "Testovych",
-      profilePhoto:
-            "https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg&#39;",
-      birthday: new Date("2000-05-16"),
-      nickname: "super dev",
-      likesCount: 10,
-};
