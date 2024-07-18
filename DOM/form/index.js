7f0f6b31e796f4826988c1c1e63942375fc0ae77
vXIXk <vlada.kostenko.19@gmail.com>
2024-07-18 12:40:47 +0300

    Fix issues

--- DOM/form/index.js
+++ DOM/form/index.js
@@ -1,17 +1,18 @@
 "use strict";
 
 const form = document.querySelector("form");
-const formData = {};
 
-form.onsubmit = e => {
-      e.preventDefault();
-      packInfo(formData);
+e.target.onsubmit = e => {
+    e.preventDefault();
+
+    const formData = {};
+    packInfo(formData);
 };
 
 function packInfo(obj) {
-      obj.userName = `${form.elements["first-name"].value} ${form.elements["last-name"].value}`;
-      obj.email = form.elements["email"].value;
-      obj.phoneNumber = `${form.elements["area-code"].value}-${form.elements["prefix"].value}-${form.elements["line-number"].value}`;
-      obj.messageSubject = form.elements["message-subject"].value;
-      obj.message = form.elements["message"].value.trim().replace(/\s+/g, " ");
+    obj.userName = `${form.elements["first-name"].value} ${form.elements["last-name"].value}`;
+    obj.email = form.elements["email"].value;
+    obj.phoneNumber = `${form.elements["area-code"].value}-${form.elements["prefix"].value}-${form.elements["line-number"].value}`;
+    obj.messageSubject = form.elements["message-subject"].value;
+    obj.message = form.elements["message"].value.trim().replace(/\s+/g, " ");
 }
